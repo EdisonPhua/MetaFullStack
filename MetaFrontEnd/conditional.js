303cef0fb94e3688e40f8f8b94cceefcd8d32159
EdisonPhua <edison.phua2004@gmail.com>
2023-06-29 06:30:21 +0000

    Thu, Jun 29, 2023, 2:30 PM +08:00

--- MetaFrontEnd/conditional.js
+++ MetaFrontEnd/conditional.js
@@ -1,3 +1,5 @@
+
+// If conditional statements
 var age = 10;
 if (age >= 65){
     console.log('You get your income from your pension')
@@ -10,4 +12,35 @@ else if( age<18){
 }
 else{
     console.log('The value of the age variable is non numerical')
+}
+
+
+//Swich conditional statements
+var day = 'Sunday';
+switch(day){
+    case 'Monday': 
+    console.log('Do some Monday stuff')
+    break
+    case 'Tuesday':
+        console.log('Do some Tuesday stuff')
+        break
+    case 'Wednesday':
+        console.log('Do some Wednesday stuff')
+        break
+    case 'Thursday':
+        console.log('Do some Thursday stuff')
+        break
+    case 'Friday':
+        console.log('Do some Friday stuff')
+        break
+    case 'Saturday':
+        console.log('Do some Saturday stuff')
+        break
+    case 'Sunday':
+        console.log('Do some Sunday stuff')
+        break
+    default:
+        console.log('There is no such day') 
+        break
+ 
 }
