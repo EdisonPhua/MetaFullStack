0590d37e3540d3fa206824087fc90b846986ca54
EdisonPhua <edison.phua2004@gmail.com>
2023-07-01 15:34:22 +0000

    Sat, Jul 1, 2023, 11:34 PM +08:00

---
+++ MetaFrontEnd/Repetitive.js
@@ -0,0 +1,38 @@
+//Task 1
+for( var i = 1; i < 6 ; i++){
+    console.log(i)
+}
+console.log('Counting Complete')
+
+//Task 2
+for( var i = 5; i >0; i--){
+    console.log(i)
+}
+console.log('Countdown finished!')
+
+//Task 3
+var i = 1
+while (i < 6 ){
+    console.log(i)
+    i++
+}
+console.log('Counting Complete!')
+
+//Task 4
+var i = 5
+while(i>0){
+    console.log(i)
+    i--
+}
+console.log('Countdown finished!')
+
+
+//Task 5
+var i = 2018
+while (i < 2023){
+    console.log(i)
+    i++
+
+
+}
+console.log('Print Year')
