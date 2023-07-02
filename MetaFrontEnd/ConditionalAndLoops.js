24f3207c12a741b444d93c9c224a280b1a5ac876
EdisonPhua <edison.phua2004@gmail.com>
2023-07-02 15:51:24 +0000

    Sun, Jul 2, 2023, 11:51 PM +08:00

--- MetaFrontEnd/ConditionalAndLoops.js
+++ MetaFrontEnd/ConditionalAndLoops.js
@@ -24,10 +24,13 @@ for ( var i = 1; i < 11; i++){
     switch (i) {
         case 1:
             console.log('Gold Medal')
+            break
         case 2:
             console.log('Silver Medal')
+            break
         case 3:
             console.log('Bronze Medal')
+            break
 
         default:
             console.log(i)
