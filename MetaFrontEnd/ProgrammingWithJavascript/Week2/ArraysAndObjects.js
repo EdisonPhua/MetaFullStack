2186352e4d298e64c20d12f0ea77852eb208a019
EdisonPhua <edison.phua2004@gmail.com>
2023-07-04 15:59:19 +0000

    Tue, Jul 4, 2023, 11:59 PM +08:00

--- MetaFrontEnd/ProgrammingWithJavascript/Week2/ArraysAndObjects.js
+++ MetaFrontEnd/ProgrammingWithJavascript/Week2/ArraysAndObjects.js
@@ -1,2 +1,18 @@
 var clothes = []
-clothes.push('')
+clothes.push('Shirt')
+clothes.push('Pants')
+clothes.push('Cap')
+clothes.push('Underwear')
+clothes.push('Belt')
+
+clothes.pop()
+
+clothes.push('Cooler Belt')
+
+console.log(clothes)
+
+var favcar = {}
+favcar.color = 'Green'
+favcar.convertible = false
+
+console.log(favcar)
