5384090403b7214f422d29e2429a7a46070358cb
EdisonPhua <edison.phua2004@gmail.com>
2023-07-05 15:08:01 +0000

    Wed, Jul 5, 2023, 11:08 PM +08:00

--- MetaFrontEnd/ProgrammingWithJavascript/Week2/ErrorPRevention.js
+++ MetaFrontEnd/ProgrammingWithJavascript/Week2/ErrorPRevention.js
@@ -6,11 +6,23 @@ addTwoNums(5, '5')
 
 function addTwoNums(a,b){
     try{
-       console.log(a+b) 
+        if(typeof(a) != 'number'){
+            throw new ReferenceError('the first argument is not a number')
+        }
+        else if(typeof(b) != 'number')
+        {
+            throw new ReferenceError('the second argument is not a number')
+        }
+        else{
+            console.log(a+b)
+        }
+        
     }
     catch(err){
-        console.log(err)
-        
+        console.log("Error!", err)      
     }
     
 }
+
+addTwoNums(5, '5')
+console.log('It still works')
