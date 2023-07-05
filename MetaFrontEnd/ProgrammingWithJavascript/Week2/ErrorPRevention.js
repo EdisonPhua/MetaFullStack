ce2cce4af30227e8e9bcca5f8bdd23ddc3cd897d
EdisonPhua <edison.phua2004@gmail.com>
2023-07-05 15:02:23 +0000

    Wed, Jul 5, 2023, 11:02 PM +08:00

---
+++ MetaFrontEnd/ProgrammingWithJavascript/Week2/ErrorPRevention.js
@@ -0,0 +1,16 @@
+function addTwoNums(a,b){
+    console.log(a+b)
+}
+
+addTwoNums(5, '5')
+
+function addTwoNums(a,b){
+    try{
+       console.log(a+b) 
+    }
+    catch(err){
+        console.log(err)
+        
+    }
+    
+}
