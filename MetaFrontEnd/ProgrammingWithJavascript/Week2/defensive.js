933044833e2e4a8e8395964675cf055cafb14f34
EdisonPhua <edison.phua2004@gmail.com>
2023-07-05 15:17:03 +0000

    Wed, Jul 5, 2023, 11:17 PM +08:00

---
+++ MetaFrontEnd/ProgrammingWithJavascript/Week2/defensive.js
@@ -0,0 +1,13 @@
+function letterFinder(word, match) {
+    var conditional1 = typeof(word) == 'string' && word.length >= 2
+    var conditional2 = typeof(match) == 'string' && match.length == 1
+    if(conditional1 == true && conditional2 == true)
+    for(i = 0; i < word.length; i++) {
+        if(word[i] == match) {
+            //if the current character at position i in the word is equal to the match
+            console.log('Found the', match, 'at', i)
+        } else {
+            console.log('---No match found at', i)
+        }
+    }
+}
