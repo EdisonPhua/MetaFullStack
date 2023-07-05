14ae0bcaec53f0787ba25967c2c439df091645ab
EdisonPhua <edison.phua2004@gmail.com>
2023-07-05 15:21:09 +0000

    Wed, Jul 5, 2023, 11:21 PM +08:00

--- MetaFrontEnd/ProgrammingWithJavascript/Week2/defensive.js
+++ MetaFrontEnd/ProgrammingWithJavascript/Week2/defensive.js
@@ -1,13 +1,20 @@
 function letterFinder(word, match) {
     var conditional1 = typeof(word) == 'string' && word.length >= 2
     var conditional2 = typeof(match) == 'string' && match.length == 1
-    if(conditional1 && conditional2)
-    for(i = 0; i < word.length; i++) {
-        if(word[i] == match) {
-            //if the current character at position i in the word is equal to the match
-            console.log('Found the', match, 'at', i)
-        } else {
-            console.log('---No match found at', i)
-        }
+    if(conditional1 && conditional2){
+        for(i = 0; i < word.length; i++) {
+            if(word[i] == match) {
+                //if the current character at position i in the word is equal to the match
+                console.log('Found the', match, 'at', i)
+            } else {
+                console.log('---No match found at', i)
+            }
+        }        
     }
+    else{
+        console.log('Please pass correct arguments to the function')
+    }
+
 }
+
+letterFinder('cat', 'c')
