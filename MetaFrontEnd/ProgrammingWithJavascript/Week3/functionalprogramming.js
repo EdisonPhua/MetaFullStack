e0209410b5f4ef2f699c52731acb2488bd69160b
EdisonPhua <edison.phua2004@gmail.com>
2023-07-06 03:45:49 +0000

    Thu, Jul 6, 2023, 11:45 AM +08:00

--- MetaFrontEnd/ProgrammingWithJavascript/Week3/functionalprogramming.js
+++ MetaFrontEnd/ProgrammingWithJavascript/Week3/functionalprogramming.js
@@ -1,11 +1,10 @@
 // Task 1: Build a function-based console log message generator
 function consoleStyler(color, background,fontSize, txt) {
     var message = "%c" + txt;
-    var style = `color: ${color};`
+    var style = `color: ${color};`;
     style += `background: ${background}; `
     style += `font-size: ${fontSize}`
-    console.log(message)
-    console.log(style)
+    console.log(message,style)
 
 }
 
