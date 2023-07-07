f814c1dfc6bfa7b48aa2b7a4199ac63fb4bc5b78
EdisonPhua <edison.phua2004@gmail.com>
2023-07-07 16:04:05 +0000

    Sat, Jul 8, 2023, 12:04 AM +08:00

--- MetaFrontEnd/ProgrammingWithJavascript/Week3/sandbox.js
+++ MetaFrontEnd/ProgrammingWithJavascript/Week3/sandbox.js
@@ -1,2 +1,30 @@
-var hello;
-console.log(hello)
+class Parent {
+    constructor(name, hair){
+        this.name = name
+        this.hair = hair
+    }
+
+    introduction(){
+        console.log(`Hello my name is ${this.name}`)
+
+    }
+}
+
+
+class Child extends Parent {
+    constructor(name, hair, toys){
+        super(name,hair)
+        this.FavToys = toys
+    }
+
+    intro(){
+        super.introduction
+        console.log(`My Favorite toy is ${this.FavToys}, my hair is ${this.hair}`)
+    }
+}
+
+
+
+
+var child1 = new Child('Alice', 'blonde', 'Barbie')
+child1.intro()
