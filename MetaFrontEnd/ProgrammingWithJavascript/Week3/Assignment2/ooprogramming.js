ef74680b22ff9f05e6496f111f782d2fa15cbd32
EdisonPhua <edison.phua2004@gmail.com>
2023-07-12 06:22:35 +0000

    Wed, Jul 12, 2023, 2:22 PM +08:00

---
+++ MetaFrontEnd/ProgrammingWithJavascript/Week3/Assignment2/ooprogramming.js
@@ -0,0 +1,29 @@
+// Task 1: Code a Person class
+class Person{
+    constructor(name="Tom", age=20, energy = 100){
+        this.name = name
+        this.age = age
+        this.energy = energy
+    }
+    sleep(){
+        this.energy += 10
+    
+    }
+    dosomethingfun(){
+        this.energy -= 10
+    }
+
+}
+// Task 2: Code a Worker class
+class worker extends Person{
+    constructor(name, age,energy)
+}
+// Task 3: Code an intern object, run methods
+function intern() {
+
+}
+
+// Task 4: Code a manager object, methods
+function manager() {
+    
+}
