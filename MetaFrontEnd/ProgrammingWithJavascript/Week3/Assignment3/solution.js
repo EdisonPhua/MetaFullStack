da33049772a56cbd74693b10dcf83315f401b7db
EdisonPhua <edison.phua2004@gmail.com>
2023-07-13 03:39:51 +0000

    Thu, Jul 13, 2023, 11:39 AM +08:00

--- MetaFrontEnd/ProgrammingWithJavascript/Week3/Assignment3/solution.js
+++ MetaFrontEnd/ProgrammingWithJavascript/Week3/Assignment3/solution.js
@@ -1,9 +1,11 @@
 // Task 1
-
+var dairy = ['cheese', 'sour cream', 'milk', 'yogurt', 'ice cream', 'milkshake'];
 function logDairy(array){
     for( dairy of array)
     console.log(dairy)
 }
+
+logDairy(dairy)
 
 // Task 2
 const animal = {
@@ -18,6 +20,16 @@ const animal = {
     
     bird.hasFeathers = true;
 
-    
+
+function birdcan(bird){
+    for( key of Object.keys(bird))
+        console.log(`${key}:${bird[key]}`)
+}
+
+birdcan(bird)
 // Task 3
+function animalCan(bird){
+    for(key in bird)
+}
+
 
