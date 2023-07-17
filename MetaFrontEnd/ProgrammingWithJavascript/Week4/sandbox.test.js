20d3ad39996f061f1671397bc01ae405f0894962
EdisonPhua <edison.phua2004@gmail.com>
2023-07-17 15:07:19 +0000

    Mon, Jul 17, 2023, 11:07 PM +08:00

--- MetaFrontEnd/ProgrammingWithJavascript/Week4/sandbox.test.js
+++ MetaFrontEnd/ProgrammingWithJavascript/Week4/sandbox.test.js
@@ -4,6 +4,6 @@ const addFive = require('./sandbox')
 test("should first", () => {
   second;
 });
-test("should first", () => {
-  second;
+test("returns the number plus 5", () => {
+  expect(addFive(1)).tobe(6);
 });
