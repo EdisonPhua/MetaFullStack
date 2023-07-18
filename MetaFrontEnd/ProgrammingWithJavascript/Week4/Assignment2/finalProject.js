ca378966574f56dd3c4d4b96366573358de9774b
EdisonPhua <edison.phua2004@gmail.com>
2023-07-18 06:01:26 +0000

    Tue, Jul 18, 2023, 2:01 PM +08:00

--- MetaFrontEnd/ProgrammingWithJavascript/Week4/Assignment2/finalProject.js
+++ MetaFrontEnd/ProgrammingWithJavascript/Week4/Assignment2/finalProject.js
@@ -21,7 +21,20 @@ const tax = 1.20;
 
 // Implement getPrices()
 function getPrices(taxBoolean) {
-    
+    for(dish of dishData){
+        var finalPrice;
+        if( taxBoolean == true){
+           finalPrice = dish * tax
+        }
+        else if(taxBoolean == false){
+            finalPrice = dish
+        }
+        else{
+            console.log('You neeed to pass a boolean to the getPrices call!')
+            return
+        }
+        console.log(`Dish: `)
+    }
 }
 
 // Implement getDiscount()
